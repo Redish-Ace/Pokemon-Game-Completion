ed33c53f21446725f2680fc94ca947dbc91af411
Lilian <lilianginga86@gmail.com>
2026-05-27 19:36:46 +0300

    Made changes to the UI

--- app/src/main/java/com/example/pokecompletion/GameAdapter.java
+++ app/src/main/java/com/example/pokecompletion/GameAdapter.java
@@ -1,6 +1,7 @@
 package com.example.pokecompletion;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
@@ -32,6 +35,24 @@ public class GameAdapter extends ArrayAdapter<GameData> {
     JSONObject jsonObject;
     String filename = "completed_tasks.json";
 
+    int[][] themes = { {R.color.red_card, R.color.red_text, R.color.red_spinner}, {R.color.blue_card, R.color.blue_text, R.color.blue_spinner}, {R.color.yellow_card, R.color.yellow_text, R.color.yellow_spinner},
+            {R.color.gold_card, R.color.gold_text, R.color.gold_spinner}, {R.color.silver_card, R.color.silver_text, R.color.silver_spinner}, {R.color.crystal_card, R.color.crystal_text, R.color.crystal_spinner},
+            {R.color.ruby_card, R.color.ruby_text, R.color.ruby_spinner}, {R.color.sapphire_card, R.color.sapphire_text, R.color.sapphire_spinner}, {R.color.emerald_card, R.color.emerald_text, R.color.emerald_spinner},
+            {R.color.firered_card, R.color.firered_text, R.color.firered_spinner}, {R.color.leafgreen_card, R.color.leafgreen_text, R.color.leafgreen_spinner},
+            {R.color.diamond_card, R.color.diamond_text, R.color.diamond_spinner}, {R.color.pearl_card, R.color.pearl_text, R.color.pearl_spinner}, {R.color.platinum_card, R.color.platinum_text, R.color.platinum_spinner},
+            {R.color.heartgold_card, R.color.heartgold_text, R.color.heartgold_spinner}, {R.color.soulsilver_card, R.color.soulsilver_text, R.color.soulsilver_spinner},
+            {R.color.black_card, R.color.black_text, R.color.black_spinner}, {R.color.white_card, R.color.white_text, R.color.white_spinner},
+            {R.color.black2_card, R.color.black2_text, R.color.black2_spinner}, {R.color.white2_card, R.color.white2_text, R.color.white2_spinner},
+            {R.color.x_card, R.color.x_text, R.color.x_spinner}, {R.color.y_card, R.color.y_text, R.color.y_spinner},
+            {R.color.omegaruby_card, R.color.omegaruby_text, R.color.omegaruby_spinner}, {R.color.alphasapphire_card, R.color.alphasapphire_text, R.color.alphasapphire_spinner},
+            {R.color.sun_card, R.color.sun_text, R.color.sun_spinner}, {R.color.moon_card, R.color.moon_text, R.color.moon_spinner},
+            {R.color.ultrasun_card, R.color.ultrasun_text, R.color.ultrasun_spinner}, {R.color.ultramoon_card, R.color.ultramoon_text, R.color.ultramoon_spinner},
+            {R.color.pikachu_card, R.color.pikachu_text, R.color.pikachu_spinner}, {R.color.eevee_card, R.color.eevee_text, R.color.eevee_spinner},
+            {R.color.sword_card, R.color.sword_text, R.color.sword_spinner}, {R.color.shield_card, R.color.shield_text, R.color.shield_spinner},
+            {R.color.brilliantdiamond_card, R.color.brilliantdiamond_text, R.color.brilliantdiamond_spinner}, {R.color.shinningpearl_card, R.color.shinningpearl_text, R.color.shinningpearl_spinner}, {R.color.arceus_card, R.color.arceus_text, R.color.arceus_spinner},
+            {R.color.scarlet_card, R.color.scarlet_text, R.color.scarlet_spinner}, {R.color.violet_card, R.color.violet_text, R.color.violet_spinner}, {R.color.za_card, R.color.za_text, R.color.za_spinner},
+            {R.color.winds_card, R.color.winds_text, R.color.winds_spinner}, {R.color.waves_card, R.color.waves_text, R.color.waves_spinner} };
+
     public GameAdapter(@NonNull Context context, @NonNull List<GameData> objects) {
         super(context, R.layout.game_item, R.id.gameName, objects);
         readTaskJSON();
@@ -50,6 +71,11 @@ public class GameAdapter extends ArrayAdapter<GameData> {
         TextView gameName = view.findViewById(R.id.gameName);
         Spinner gameTasks = view.findViewById(R.id.taskSpinner);
 
+        if(position < themes.length) {
+            view.setBackgroundResource(themes[position][0]);
+            gameName.setTextColor(ContextCompat.getColor(this.getContext(), themes[position][1]));
+            gameTasks.setBackgroundResource(themes[position][2]);
+        }
         assert gameData != null;
         gameName.setText(gameData.name);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item, gameData.tasks);
